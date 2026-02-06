4aa3a2572a1fc79b714036b5aab4fee1ca1ea975
Martin <momartelo@yahoo.com.ar>
2026-02-05 23:43:49 -0300

    colores paginas a construir

--- src/app/dolar/page.jsx
+++ src/app/dolar/page.jsx
@@ -2,15 +2,15 @@ import React from "react";
 
 export default function DolarPage() {
   return (
-    <main className="min-h-[calc(100vh-64px)] flex items-center justify-center bg-gray-50 dark:bg-gray-900 px-4">
-      <div className="text-center bg-white dark:bg-gray-800 p-8 rounded-xl shadow max-w-md w-full">
+    <main className="min-h-[calc(100vh-64px)] flex items-center justify-center   px-4">
+      <div className="text-center bg-white p-8 rounded-xl border border-gray-300 shadow max-w-md w-full">
         <div className="text-5xl mb-4 animate-bounce">🚧</div>
 
-        <h1 className="text-2xl font-semibold text-gray-900 dark:text-gray-100 mb-2">
+        <h1 className="text-2xl font-semibold text-gray-900  mb-2">
           Página en construcción
         </h1>
 
-        <p className="text-gray-600 dark:text-gray-400">
+        <p className="text-gray-600 ">
           🛠 Estamos trabajando para traerte esta sección muy pronto.
         </p>
       </div>
